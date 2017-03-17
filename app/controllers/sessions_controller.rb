3ed19b4836b62dc266f32acdd24d85745c22f496
Aecio Levy <aeciolevy@gmail.com>
2017-03-17 23:33:47 +0000

    tryed to redirect to back url, not finish.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
-      session[:user_id] = user.id
       flash[:notice] = "Successfully logged in"
+      session[:user_id] = user.id
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
