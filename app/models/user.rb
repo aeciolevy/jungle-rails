3cc1e91a4101e6d866b9208c00cd077113d95739
Aecio Levy <aeciolevy@gmail.com>
2017-03-21 00:32:16 +0000

    Added spec for class method authentication with credentials, handle sensitive email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
   validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, length: {minimum: 6}
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    new_email = email.strip
+    lower_email = new_email.downcase
+    user = User.find_by_email(lower_email)
     if user && user.authenticate(password)
       user
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
       @user = User.new
       @user.first_name = 'Ronald'
       @user.last_name = 'Newton'
-      @user.email = 'ronald@newton.com'
+      @user.email = 'ronald1@newton.com'
       @user.password = '123456'
     end
 
@@ -59,8 +59,29 @@ RSpec.describe User, type: :model do
     end
   end
 
-  # describe '.authenticate_with_credentials' do
+  describe '.authenticate_with_credentials' do
+    it 'Email is blank' do
+      expect(User.authenticate_with_credentials('', '12345')).to eql(nil)
+    end
+    it 'Email does not exist' do
+      expect(User.authenticate_with_credentials('ae@aol.com', '12345')).to eql(nil)
+    end
+    it 'There is email but password is wrong' do
+      expect(User.authenticate_with_credentials('isaac@newton.com', '123123')).to eql(nil)
+    end
+    it 'There is email but password is blank' do
+      expect(User.authenticate_with_credentials('isaac@newton.com', '')).to eql(nil)
+    end
+    it 'Normal authentication' do
+      expect(user = User.authenticate_with_credentials('ronald@newton.com', '123456')).to eql(user)
+    end
+    it 'Authentication with few spaces' do
+      expect(user = User.authenticate_with_credentials('  ronald@newton.com  ', '123456')).to_not eql(nil)
+    end
+    it 'Authentication with few spaces' do
+      expect(user = User.authenticate_with_credentials('  rOnAld@Newton.com  ', '123456')).to_not eql(nil)
+    end
 
-  # end
+  end
 
 end
