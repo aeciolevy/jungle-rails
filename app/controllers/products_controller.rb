09dc6478a20f90b22f84086cdfb78847d30b11d2
Aecio Levy <aeciolevy@gmail.com>
2017-03-24 01:47:17 +0000

    Refactor queries in the product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,32 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products = Product.order(created_at: :desc)
+    @average = calculate_average
   end
 
   def show
     @product = Product.find params[:id]
     @new_review = @product.reviews.new
     @reviews = Review.where(product_id: @product.id).order(:created_at).reverse_order
+  end
+
+  private
+
+  def calculate_average
+    @products = Product.all.order(created_at: :desc)
+    rate_hash = {}
+    @products.each do |prod|
+      @reviews = Review.where(product_id: prod.id)
+      sum = 0
+      average = 0
+      @reviews.each do |rev|
+        sum = sum + rev.rating
+      end
+      average = sum.to_f / @reviews.size.to_f
+      rate_hash[prod.id] = average
+    end
+    rate_hash
   end
 
 end
